1c21512c2c54e9dc5952eef9850e04ab1c2ff83c
solashirai <solashakashirai@gmail.com>
2020-09-11 11:00:35 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='frex',
+    version='0.1',
+    description='Package for Explainable Recommender Framework.',
+    url='git@github.com:solashirai/ExplainableRecommenderFramework.git',
+    author='Sola S. Shirai',
+    packages=['frex']
+)
