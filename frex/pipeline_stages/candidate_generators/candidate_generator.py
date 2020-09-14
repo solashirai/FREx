670b168706031c75b951ffea56b959493894e353
solashirai <solashakashirai@gmail.com>
2020-09-14 09:21:13 -0400

    only convert to sparse matrix if the vectors are sparse

--- frex/pipeline_stages/candidate_generators/candidate_generator.py
+++ frex/pipeline_stages/candidate_generators/candidate_generator.py
@@ -46,12 +46,15 @@ class CandidateGenerator(_PipelineStage):
         """
         ind_to_item = dict()
         content_matrix = np.zeros(shape=(len(comparison_items), target_vector.shape[1]))
-        content_matrix = lil_matrix(content_matrix)
+
+        if isinstance(comparison_contents[0], csr_matrix):
+            content_matrix = lil_matrix(content_matrix)
         for item_ind, item in enumerate(comparison_items):
             ind_to_item[item_ind] = item
             content_matrix[item_ind] = comparison_contents[item_ind]
 
-        content_matrix = content_matrix.tocsr()
+        if isinstance(content_matrix, lil_matrix):
+            content_matrix = content_matrix.tocsr()
         cosine_sims: List[float] = CandidateGenerator.cosine_sim(
             comparison_vector=target_vector, comparison_matrix=content_matrix
         ).tolist()[0]
