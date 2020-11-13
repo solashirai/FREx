9f7b8a5bc02f6ca6791a05c2f7d7f74d4f248560
solashirai <solashakashirai@gmail.com>
2020-11-13 11:48:02 -0500

    add missing documentation

--- frex/models/constraint_section_solution.py
+++ frex/models/constraint_section_solution.py
@@ -7,6 +7,13 @@ from frex.models import Candidate
 @dataclass_json
 @dataclass
 class ConstraintSectionSolution:
+    """
+    A 'section' of a constraint solution. Exactly what a section represents may differ based on the application,
+    but an example of a section is a 'day' within a solution that is generated for a week-long meal plan.
+    Sections have their own score, and store the candidates assigned to this section. Values of attributes of the
+    domain objects are also stored, if they were relevant to solving constraints.
+    """
+
     section_score: int
     section_attribute_values: Dict[str, int]
     section_candidates: Tuple[Candidate, ...]
--- frex/models/constraint_solution.py
+++ frex/models/constraint_solution.py
@@ -7,6 +7,11 @@ from frex.models import ConstraintSectionSolution
 @dataclass_json
 @dataclass
 class ConstraintSolution:
+    """
+    A constraint solution is the combination of multiple sections, each containing some number of items, that adhere
+    to some number of constraints while maximizing the total score of all items contained in the solution.
+    """
+
     overall_score: int
     overall_attribute_values: Dict[str, int]
     sections: Tuple[ConstraintSectionSolution, ...]
