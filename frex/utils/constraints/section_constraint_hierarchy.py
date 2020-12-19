cd47ae330f897fe7352d89f62c73697aeb8a678e
solashirai <solashakashirai@gmail.com>
2020-12-19 12:39:18 -0500

    update typing for hierarchy of constraints

--- frex/utils/constraints/section_constraint_hierarchy.py
+++ frex/utils/constraints/section_constraint_hierarchy.py
@@ -1,15 +1,18 @@
 from __future__ import annotations
-from typing import NamedTuple, Optional
+from typing import Tuple, Optional
 from rdflib import URIRef
-from frex.utils.constraints import ConstraintType
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 
 
-class SectionConstraintHierarchy(NamedTuple):
+@dataclass_json
+@dataclass
+class SectionConstraintHierarchy:
     """
     The SectionConstraintHierarchy is used to capture a hierarchy of relations among sections. This primarily
     is used to capture logical AND / OR operators among relations.
     This currently only correctly supports hierarchies of sections as trees (?)
     """
     root_uri: URIRef
-    dependency_and: Optional[SectionConstraintHierarchy] = None
-    dependency_or: Optional[SectionConstraintHierarchy] = None
+    dependency_and: Optional[Tuple[SectionConstraintHierarchy, ]] = ()
+    dependency_or: Optional[Tuple[SectionConstraintHierarchy, ...]] = ()
