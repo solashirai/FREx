1cb542077eae1d89303fa835d3630c9a6e9ba2f3
solashirai <solashakashirai@gmail.com>
2021-04-13 15:57:47 -0400

    make explanation dataclass frozen

--- frex/models/explanation.py
+++ frex/models/explanation.py
@@ -3,7 +3,7 @@ from dataclasses_json import dataclass_json
 
 
 @dataclass_json
-@dataclass
+@dataclass(frozen=True)
 class Explanation:
     """
     An Explanation in its most simple form consists of a string.
