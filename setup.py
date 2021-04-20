a783f60d9e587baf4a21525f9a5c53200a19632a
Sola Shirai <solashakashirai@gmail.com>
2021-04-20 08:27:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="frex",
-    version="0.5",
+    version="1.0",
     description="Package for Explainable Recommender Framework.",
     url="git@github.com:solashirai/ExplainableRecommenderFramework.git",
     author="Sola S.D. Shirai",
